b042f2b50a4887565cc1ed651f50dba5b462280e
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-05-21 11:04:01 -0700

    adding setmode

--- main.ts
+++ main.ts
@@ -2,4 +2,9 @@ namespace ws2812b {
     //% shim=sendBufferAsm
     export function sendBuffer(buf: Buffer, pin: DigitalPin) {
     }
+
+    //% shim=setBufferMode
+    export function setMode(pin: DigitalPin, mode: number) {
+
+    }
 }
