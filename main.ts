592f16968c7698303769da01e962ebf6b1d9a261
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-04-27 09:14:45 -0700

    add constants

--- main.ts
+++ main.ts
@@ -7,4 +7,9 @@ namespace ws2812b {
     export function setBufferMode(pin: DigitalPin, mode: number) {
 
     }
+
+    export const BUFFER_MODE_RGB = 1
+    export const BUFFER_MODE_RGBW = 2
+    export const BUFFER_MODE_RGB_RGB = 3
+    export const BUFFER_MODE_AP102 = 4
 }
